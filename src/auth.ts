4c77dac48daabbae57120acf8638c976d587feb4
Matt Strick <matt.strick@gmail.com>
2024-10-24 08:40:27 -0500

    reverting last commit

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,5 @@ import NextAuth from "next-auth";
 import TikTok from "next-auth/providers/tiktok";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    TikTok({authorization: { params: { scope: "user.info.basic" } }})
-],
+  providers: [TikTok],
 });
