ed9667eda9133f17dabaa559ff47fb09220c4172
Matt Strick <matt.strick@gmail.com>
2024-10-24 15:27:19 -0500

    Added an under construction icon to the TikTok sign in

--- src/app/components/LeftColumn.tsx
+++ src/app/components/LeftColumn.tsx
@@ -5,7 +5,7 @@ export default function LeftColumn() {
     return (
       <div className="p-8 bg-[#8D8E94]">
           <h1 className="text-3xl font-bold">Platforms</h1>
-          <div className="flex">TikTok -{" "}<TikTokSignIn /></div>
+          <div className="flex">🚧 TikTok -{" "}<TikTokSignIn /></div>
           <div className="flex">Google -{" "}<GoogleSignIn /></div>
       </div>
     );
