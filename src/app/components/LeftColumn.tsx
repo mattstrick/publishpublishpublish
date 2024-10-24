321684cfe23ee5e42e5cd38cf4f48b059ecd470e
Matt Strick <matt.strick@gmail.com>
2024-10-24 15:45:30 -0500

    Testing what information we get from the session object from Google

--- src/app/components/LeftColumn.tsx
+++ src/app/components/LeftColumn.tsx
@@ -1,12 +1,16 @@
 import GoogleSignIn from "./google-sign-in";
 import TikTokSignIn from "./tiktok-sign-in";
+import { auth } from "@/auth";
 
-export default function LeftColumn() {
+export default async function LeftColumn() {
+    const session = await auth()
+console.log('HIIIIII', session);
     return (
       <div className="p-8 bg-[#8D8E94]">
           <h1 className="text-3xl font-bold">Platforms</h1>
           <div className="flex">🚧 TikTok -{" "}<TikTokSignIn /></div>
           <div className="flex">Google -{" "}<GoogleSignIn /></div>
+
       </div>
     );
   }
